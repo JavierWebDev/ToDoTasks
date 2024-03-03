022802cb37ebb693fa94257686325e99fd3c2177
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-03-02 20:59:22 -0500

    Added Priority Selection (Front-End)

--- App/Components/checkTasks/CheckTasks.js
+++ App/Components/checkTasks/CheckTasks.js
@@ -1,11 +1,24 @@
-export class MarcarTarea extends HTMLElements {
+export class MarcarTarea extends HTMLElement {
 	constructor() {
 		super();
 		this.render();
 	}
 	render() {
 		this.innerHTML = /* html */ `
-		CODIGO HTML
+		<section id="SectionMarcar" class="contenedor-marcar_tareas">
+			<h2 class="contenedor-subtitulo">Gestor De Tareas</h2>
+
+			<div id="ContenedorMarcarEstado" class="contenedor-tareas_select">
+				<div id="Tarea" class="tarea-check prioridad-baja">
+					<h3 class="nombre-tarea" id="NombreTarea">xxxx</h3>
+
+					<div id="ContenedorSeleccionar" class="contenedor-select_check">
+						<button id="BotonCompletada" class="seleccionar-btn"><box-icon name='check-circle' color='#56d324' ></box-icon></button>
+						<button id="BotonPendiente" class="seleccionar-btn"><box-icon name='x-circle' color='#ec2a13'></box-icon></button>
+					</div>
+				</div>
+			</div>
+		</section>
 		`
 	}
 }
